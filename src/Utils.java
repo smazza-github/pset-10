fb794586ce4824850b7cb9235e01471d4a02b336
smazza@ucvts.org <smazza@ucvts.org>
2020-03-13 18:35:09 -0400

    Started Util.java

---
+++ src/Utils.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+public class Utils {
+
+    public static String getClasspathDir() {
+    	
+        String classpath = System.getProperty("java.class.path", ".");
+        boolean windows = false;
+        
+        if (classpath.matches(".*\\\\.*")) {
+        
+            windows = true;
+            
+        }
+        
+        if (windows) {
+        	
+            String[] splitClasspathDir = classpath.split(";");
+            String classpathDirectory = "";
+            for (String s : splitClasspathDir) {
+                if (s.matches(".*lib\\\\.*")) {
+                    classpathDirectory = s;
+                    
+                }
+            }
+            
+            return classpathDirectory;
+            
+        } else {
+        	
+            String[] splitClasspathDir = classpath.split(":");
+            String classpathDirectory = "";
+            
+            for (String s : splitClasspathDir) {
+            	
+                if (s.matches(".*lib/.*")) {
+                    classpathDirectory = s;
+                    
+                }
+            }
+            
+            return classpathDirectory;
+            
+        }
+    }
