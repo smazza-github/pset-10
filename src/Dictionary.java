8db2064fb675e63edb52cc13b3d28d0b470d6389
smazza@ucvts.org <smazza@ucvts.org>
2020-03-13 23:13:41 -0400

    dictionary header

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -580,3 +580,29 @@ private void initialize() throws FileNotFoundException, BadLocationException {
             
           }
         }
+        
+        list.setModel(DLM);
+        txtSearch.setText("");
+        doc.remove(0, doc.getLength());
+        
+        doc.insertString(doc.getLength(),"Example Word\n" ,bigWord );
+          doc.insertString(doc.getLength(),"\n" , null );
+          doc.insertString(doc.getLength(),"Definitions\n" ,header );
+          doc.insertString(doc.getLength(),"\n" ,null );
+          doc.insertString(doc.getLength(),"1. Example Word (pos) \n\n    Definition of example word\n\n" ,null );
+          doc.insertString(doc.getLength(),"\n" ,null );
+          doc.insertString(doc.getLength(),"Synonyms\n" ,header );
+          doc.insertString(doc.getLength(),"\n1.Synonym " ,null );
+          doc.insertString(doc.getLength(),"\n\n" ,null );
+          doc.insertString(doc.getLength(),"Antonyms\n" ,header );
+          doc.insertString(doc.getLength(),"\n1.Antonym " ,null );
+          
+      } catch (BadLocationException e) {
+
+        e.printStackTrace();
+        
+      }
+        }
+      });
+    
+    
