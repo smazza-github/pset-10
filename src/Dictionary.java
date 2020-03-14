979af6212c11862d5689be14a192a3aaf9b4b181
smazza@ucvts.org <smazza@ucvts.org>
2020-03-13 21:27:41 -0400

    Word panel, label, and textfield

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,5 +1,7 @@
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,6 +10,7 @@ import java.util.ArrayList;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
@@ -109,4 +112,21 @@ private void initialize() throws FileNotFoundException, BadLocationException {
     
     JScrollPane scrollPane_3 = new JScrollPane();
     panel.add(scrollPane_3, "addWord");
+    
+    JPanel panel_1 = new JPanel();
+    panel_1.setToolTipText("");
+    panel_1.setBackground(Color.WHITE);
+    scrollPane_3.setViewportView(panel_1);
+    panel_1.setLayout(null);
+    
+    JLabel lblNewLabel = new JLabel("Word*");
+    lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
+    lblNewLabel.setBounds(10, 11, 117, 54);
+    panel_1.add(lblNewLabel);
+    
+    textField = new JTextField();
+    textField.setToolTipText("word");
+    textField.setBounds(20, 76, 286, 20);
+    panel_1.add(textField);
+    textField.setColumns(10);
         
