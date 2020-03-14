e0c58c9c34794fc6d4b83594718710d6ffbe6447
smazza@ucvts.org <smazza@ucvts.org>
2020-03-13 20:01:39 -0400

    Words

--- src/Words.java
+++ src/Words.java
@@ -3,3 +3,10 @@ public class Words {
     private Definitions[] definitions;
     private String[] synonyms;
     private String[] antonyms;
+   
+    public Words(String word, Definitions[] definitions, String[] synonyms, String[] antonyms) {
+        this.word = word;
+        this.definitions = definitions;
+        this.synonyms = synonyms;
+        this.antonyms = antonyms;
+    }
