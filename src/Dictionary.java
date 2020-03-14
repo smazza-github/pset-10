6aebb180f161bdb4cae605ce8635a856033e7ab5
smazza@ucvts.org <smazza@ucvts.org>
2020-03-13 20:32:07 -0400

    Created JTextFields

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,3 +1,16 @@
-public class Dictionary{
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Dictionary {
 	
+	  private JFrame frmDictionary;
+	  private JTextField txtSearch;
+	  private final ButtonGroup buttonGroup = new ButtonGroup();
+	  private JTextField textField;
+	  private JTextField txtDefinitions;
+	  private JTextField textField_2;
+	  private JTextField textField_1;
+	  private JTextField textField_3;
+	  
 }
